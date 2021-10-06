8360312c0c37c97587e01c6933c50f2269c50815
Rex Walters <rw@pobox.com>
2021-10-05 19:29:35 -0700

    user script works on example.com

--- gbWidgets.user.js
+++ gbWidgets.user.js
@@ -15,5 +15,27 @@
   const wrapper = document.createElement("div");
   document.body.appendChild(wrapper);
 
-  wrapper.innerHTML = `<h1>Hi Mom!</h1>`;
+  wrapper.innerHTML = `
+<style>
+  section.container {
+    --fill-color: purple;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+</style> 
+<h1>Sample use of gbWidgets</h1>
+<section class="container">
+  <dial-gauge data-value="0.33" data-footer="147 total (<b>13</b> new)">Difficulty</dial-gauge>
+  <dial-gauge
+    data-value="0.63"
+    data-display="147"
+    data-footer="5 sessions, 493 reviews"
+  >Reviews/day</dial-gauge>
+  <bar-graph
+    data-values="[247,115,69,250,6,2,0,1,3]"
+    data-labels='["10s","20s","30s","1m","1.5m","2m","5m","10m",">10m"]'
+  >Review Intervals</bar-graph>
+</section>
+  `;
 })();
